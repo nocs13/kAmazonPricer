b302865d988128837baeb6329a0954c965d43253
Nocs13 <nocs13@gmail.com>
2014-07-18 19:35:02 +0400

    starting...

---
+++ src/com/kgm/kLibAmazon/kDbPricer.java
@@ -0,0 +1,15 @@
+package com.kgm.kLibAmazon;
+
+import java.io.File;
+import org.w3c.dom.*;
+
+/**
+ * Created by goga on 7/18/14.
+ */
+public class kDbPricer
+{
+    public kDbPricer()
+    {
+
+    }
+}
