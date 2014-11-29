037dcc5890cd998fdaf1356d599dd3775e57fa19
Nocs13 <nocs13@gmail.com>
2014-11-29 12:00:31 +0400

    ...

--- src/com/kgm/kAmazonPricer/Main.java
+++ src/com/kgm/kAmazonPricer/Main.java
@@ -2,6 +2,10 @@ package com.kgm.kAmazonPricer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
 
 /**
  * Created by goga on 7/3/14.
@@ -17,9 +21,18 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        base = new kViewBase(this);
-
+        //base = new kViewBase(this);
         //setContentView(base);
+        
         setContentView(R.layout.lstart);
+        
+        Button btn = (Button) findViewById(R.id.btn_lstart_start);
+        btn.setOnClickListener(new OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                setContentView(R.layout.lay_menu);
+            }
+        });
     }
 }
