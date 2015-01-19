14ebba5880561e09b30f85e1c325e1165d5a20b0
nocs13 <nocs13@gmail.com>
2015-01-19 16:17:22 +0400

    ...

--- src/com/kgm/kLibAmazon/kDbPricer.java
+++ src/com/kgm/kLibAmazon/kDbPricer.java
@@ -5,6 +5,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.w3c.dom.*;
 import org.xmlpull.v1.XmlPullParser;
@@ -21,9 +25,13 @@ public class kDbPricer
     private XmlPullParser xml_parser = null;
     
     private String dbFile = "kdbprice.db";
+    
+    private List   dbList;
 
     public kDbPricer()
     {
+    	dbList = new ArrayList();
+    	
     	try {
     		xml_object = XmlPullParserFactory.newInstance();
     		xml_parser = xml_object.newPullParser();
@@ -33,13 +41,29 @@ public class kDbPricer
         	if (data.length() < 1)
         		return;
         	
+        	xml_parser.setInput(new StringReader(data));
         	
+        	 int eventType = xml_parser.getEventType();
+
+        	 while (eventType != XmlPullParser.END_DOCUMENT) {
+              if(eventType == XmlPullParser.START_DOCUMENT) {
+                  System.out.println("Start document");
+              } else if(eventType == XmlPullParser.START_TAG) {
+                  System.out.println("Start tag "+xml_parser.getName());
+              } else if(eventType == XmlPullParser.END_TAG) {
+                  System.out.println("End tag "+xml_parser.getName());
+              } else if(eventType == XmlPullParser.TEXT) {
+                  System.out.println("Text "+xml_parser.getText());
+              }
+              eventType = xml_parser.next();
+             }
+
     	} catch (Exception e) {
     		
     	}    	
     }
     
-    public String readFile(String filename)
+    private String readFile(String filename)
     {
        String content = null;
        File file = new File(filename);
@@ -56,10 +80,9 @@ public class kDbPricer
        
        return content;
     }
-
-    public boolean update()
+    
+    private boolean writeFile(String filename, String data)
     {
-        return true;
+    	return true;
     }
-
 }
