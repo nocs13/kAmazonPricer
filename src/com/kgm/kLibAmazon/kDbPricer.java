facf940671abe7d1617582902c648010077f9f4c
nocs13 <nocs13@gmail.com>
2015-01-19 16:03:38 +0400

    ...

--- src/com/kgm/kLibAmazon/kDbPricer.java
+++ src/com/kgm/kLibAmazon/kDbPricer.java
@@ -1,31 +1,63 @@
 package com.kgm.kLibAmazon;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.w3c.dom.*;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import android.content.Context;
 
 /**
  * Created by goga on 7/18/14.
  */
 public class kDbPricer
 {
-    File db_file;
+    private XmlPullParserFactory xml_object = null;
+    private XmlPullParser xml_parser = null;
+    
+    private String dbFile = "kdbprice.db";
 
     public kDbPricer()
     {
-
+    	try {
+    		xml_object = XmlPullParserFactory.newInstance();
+    		xml_parser = xml_object.newPullParser();
+    		        	
+        	String data = readFile(dbFile);
+        	
+        	if (data.length() < 1)
+        		return;
+        	
+        	
+    	} catch (Exception e) {
+    		
+    	}    	
     }
-
-    public kDbPricer(String path)
+    
+    public String readFile(String filename)
     {
-        open(path);
+       String content = null;
+       File file = new File(filename);
+       
+       try {
+           FileReader reader = new FileReader(file);
+           char[] chars = new char[(int) file.length()];
+           reader.read(chars);
+           content = new String(chars);
+           reader.close();
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+       
+       return content;
     }
 
-    public void close()
-    {
-
-    }
-
-    public boolean open(String path)
+    public boolean update()
     {
         return true;
     }
---
+++ src/com/kgm/kLibAmazon/kDbUnit.java
@@ -0,0 +1,5 @@
+package com.kgm.kLibAmazon;
+
+public class kDbUnit {
+
+}
